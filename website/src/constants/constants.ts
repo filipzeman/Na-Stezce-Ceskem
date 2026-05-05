7b5b14fbc57322d2127626b787b51b4d70ca2b11
Filip Zeman <zeman.filip@gmail.com>
2026-05-05 06:43:06 +0200

    add map, update footer, header

--- website/src/constants/constants.ts
+++ website/src/constants/constants.ts
@@ -2,7 +2,7 @@ export const ETAPE_LENGTH: number = 93.1;
 
 export const FIRST_LEVEL_NAVIGATION: string[] = [
   "Itinerář",
-  "Mapa",
+  "Mapa etapy",
   "Časté dotazy",
   "Blog",
   "O projektu",
